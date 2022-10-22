5e05119cfa07793f0e15a140634245013c83b2a6
unknown <222navjot@gmail.com>
2022-10-22 11:31:19 +0530

    express

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import "./app.css"
+
+export default function App() {
+  
+  return <>
+  <header>
+    <p class="hello">Hello World !</p>
+    <p class="htmlcss">Hi! From Navjot</p>
+
+    <a href="https://drive.google.com/file/d/1NPfCeX0mFe4aDyB3O_Ddymp0qFynODnZ/view?usp=sharing" target="_blank">Click here to Visit My Resume</a>
+
+  </header>
+  </>
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join('client', 'dist')));
+
+const PORT = process.env.POR || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
